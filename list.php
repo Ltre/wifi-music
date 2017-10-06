51ee609a0e62cb6fb125e5bdafceed4274a0a696
Ltre <ltrele@yeah.net>
2017-10-06 23:45:51 +0800

    scan2

--- list.php
+++ list.php
@@ -5,6 +5,7 @@ ob_start();
 date_default_timezone_set('PRC');
 session_start();
 
+//递归方式（DFS)
 function scan($dir){
     $map = [];
     preg_match('/\/$/', $dir) OR $dir .= '/';
@@ -29,6 +30,36 @@ function scan($dir){
     return $map;
 }
 
-$map = scan('D:/SERV_WORKSPACE/WebstormProjects/wifi-music/sdcard');
+//非递归 (BFS)
+function scan2($dir){
+    $map = [];
+    $queue = [];
+    array_unshift($queue, $dir);
+    while (! empty($queue)) {
+        $e = array_pop($queue);
+        $e = rtrim($e, '/');
+        if (is_dir($e) && ($dh = opendir($e))) {
+            while (false !== ($file = readdir($dh))) {
+                if (in_array($file, ['.', '..'])) continue;
+                $path = "{$e}/{$file}";
+                if (is_dir($path)) {
+                    array_unshift($queue, $path);
+                } else {
+                    if (preg_match('/\.(mp3|wma|wav|m4a|ape|flac|aac)$/', $path)) {
+                        $k = sha1($path);
+                        $map[$k] = [
+                            'path' => $path,
+                            'file' => $file,
+                        ];
+                    }
+                }
+            }
+        }
+    }
+    return $map;
+}
+
+// $map = scan('D:/SERV_WORKSPACE/WebstormProjects/wifi-music/sdcard');
+$map = scan2('/Users/Ltre/mydir/projects/wifi-music/sdcard');
 file_put_contents('list.json', json_encode($map));
 echo json_encode(['map' => $map, 'len' => count($map)]);
