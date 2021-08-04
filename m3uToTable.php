10efa15d2941cd919e35f8f600012931d9e2146d
Ltre <ltrele@yeah.net>
2021-08-04 23:33:21 +0800

    1

--- m3uToTable.php
+++ m3uToTable.php
@@ -1,4 +1,13 @@
 <?php
 /**
  * save to sqlite
+ * 
+ * 相关资料：
+ *      m3u - 维基百科
+ *      https://zh.wikipedia.org/wiki/M3U
+ *      m3u文件 - 百度百科
+ *      https://baike.baidu.com/item/m3u%E6%96%87%E4%BB%B6/365977
+ *      [翻译] M3U 和M3U8 详解_from wiki
+ *      https://log.fyscu.com/index.php/archives/28/
+ *      
  */
