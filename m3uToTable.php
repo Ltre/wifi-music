cabc1cd3a16929fa96b2e3803f10a15e98c4a881
Ltre <ltrele@yeah.net>
2021-08-04 23:35:30 +0800

    1

--- m3uToTable.php
+++ m3uToTable.php
@@ -11,7 +11,7 @@
  *      https://log.fyscu.com/index.php/archives/28/
  * 
  *      #EXTM3U	文件的头部，必须是文件的第一行。	
- *      #EXTINF	指示多媒体文件的信息，包括播放时间和标题。	
+ *      #EXTINF	指示多媒体文件的信息，包括播放时间(秒)和标题。	
  *      #EXTINF:191,Artist Name - Track Title
  *      
  */
