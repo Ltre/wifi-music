08d94cf622f492512246dd84ba6c7f3298344b6d
Ltre <ltrele@yeah.net>
2021-08-04 23:34:31 +0800

    1

--- m3uToTable.php
+++ m3uToTable.php
@@ -9,5 +9,9 @@
  *      https://baike.baidu.com/item/m3u%E6%96%87%E4%BB%B6/365977
  *      [翻译] M3U 和M3U8 详解_from wiki
  *      https://log.fyscu.com/index.php/archives/28/
+ * 
+ *      #EXTM3U	文件的头部，必须是文件的第一行。	
+ *      #EXTINF	指示多媒体文件的信息，包括播放时间和标题。	
+ *      #EXTINF:191,Artist Name - Track Title
  *      
  */
