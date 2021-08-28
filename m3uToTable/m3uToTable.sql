852613ef366fa5c40c64e87f466e6f616f205bfc
Ltre Oreki <ltrele@yeah.net>
2021-08-28 20:47:39 +0800

    Update m3uToTable.sql

--- m3uToTable/m3uToTable.sql
+++ m3uToTable/m3uToTable.sql
@@ -30,3 +30,24 @@ CREATE TABLE `hist` (
 );
 
 
+
+
+
+
+
+
+
+
+
+
+-- 以下是查询范例 --
+
+
+-- 获取每首歌曲已打标签数： 
+select song_id, count(1) as cnt from song_tag group by song_id -- HAVING cnt > 1
+-- 获取至少有两个标签（1个ALL,1个别的）的歌曲：
+select * from song, (select song_id, count(1) as cnt from song_tag group by song_id HAVING cnt > 1) AS tmptable where song.id = tmptable.song_id order by tmptable.cnt DESC
+-- 获取只有ALL标签的歌曲（可以看作没打标签的）：
+select * from song, song_tag where song.id = song_tag.song_id and song_tag.tag = 'ALL' 
+-- 获取同时打了某些标签的歌曲：
+select song.* from song, (select song_id, count(1) as cnt from song_tag where tag IN ('AlarmClock', 'Epic', 'S+', '某推荐', '至喜', '舒') group by song_id HAVING cnt > 1) as tmptable where song.id = tmptable.song_id
